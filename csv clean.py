1c940c6efa6c1d1744ef3b35160fa16b0c7b115b
KhooZiKai <140942079+KhooZiKai@users.noreply.github.com>
2023-08-07 00:00:05 +0800

    Add files via upload

---
+++ csv clean.py
@@ -0,0 +1,27 @@
+import csv
+import re
+
+def clean_text(text):
+    # Define the characters to be removed
+    characters_to_remove = r'",.[]{}\/|\\-()#$%^&*!@~?><:;=+'
+
+    # Use regular expression to remove the characters
+    cleaned_text = re.sub(f'[{re.escape(characters_to_remove)}]', '', text)
+    return cleaned_text
+
+def clean_csv(input_file, output_file):
+    with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        for row in reader:
+            cleaned_row = [clean_text(cell) for cell in row]
+            writer.writerow(cleaned_row)
+
+if __name__ == '__main__':
+    input_csv_file = r"C:\Users\khooz\Downloads\NER_split\NER_only.csv"
+    output_csv_file = r"C:\Users\khooz\Downloads\NER_split\NER_clean.csv"
+
+    clean_csv(input_csv_file, output_csv_file)
+
+
---
+++ csv wordcloud.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# Read the contents of the file
+file_path = r"C:\Users\khooz\Downloads\pythoncount_output_c\part-00000"
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = file.read()
+
+# Generate the word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(data)
+
+# Display the word cloud using matplotlib
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()

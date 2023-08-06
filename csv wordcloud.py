92f7d12cb3453423b8e65062c8aa239c45269b49
KhooZiKai <140942079+KhooZiKai@users.noreply.github.com>
2023-08-07 00:18:07 +0800

    csv wordcloud.py

--- csv wordcloud.py
+++ csv wordcloud.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 # Read the contents of the file
-file_path = r"C:\Users\khooz\Downloads\pythoncount_output_c\part-00000"
+file_path = r"C:\Users\khooz\Downloads\pythoncount_output_c\part-00000"  # Only Works On My Computer, Change to you Input Path
 with open(file_path, 'r', encoding='utf-8') as file:
     data = file.read()
 
